0e5de8aaae3782e94773f8b1d882ad5a1feaf471
Juan Di Toro <ditorojuanf@gmail.com>
2019-12-14 14:31:46 -0400

    Initial server setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,20 @@
-require('dotenv').config()
-
 import mongoose from 'mongoose'
+import express from 'express'
+import bodyParser from 'body-parser'
 
+require('dotenv').config()
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true}).then(() => {
   console.warn('Connected!')
 })
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  return res.send('Hello there')
+})
+
+app.listen(8001, () =>
+  console.log('Server started on http://localhost:8001!'))
