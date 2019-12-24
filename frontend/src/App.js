adcaaf8f038706f572ff81e67677d36789e7b694
Juan Di Toro <ditorojuanf@gmail.com>
2019-12-24 11:39:45 -0400

    Fixed responsive and apartments grid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 import Item from './Item'
 import './App.css'
 
+export const AppContext = React.createContext(null)
+
 function App() {
+    const [showFilter, setShowFilter] = useState(false)
+    const [showMenu, setShowMenu] = useState(false)
     return (
         <div className="App">
-            <Header />
-            <div className="items">
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-            </div>
+            <AppContext.Provider
+                value={{
+                    showFilter: show => setShowFilter(show),
+                    showMenu: show => setShowMenu(show),
+                }}
+            >
+                <Header />
+                <div className="items">
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                </div>
+            </AppContext.Provider>
         </div>
     )
 }
